9812756e367fe5d4b5c8a5b6aa780a394f9eb142
conficiusa <addawebadua@gmail.com>
2025-03-19 18:23:26 +0000

    fix font subsets

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,4 +2,5 @@ import { Geist } from "next/font/google";
 
 export const geist = Geist({
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin", "latin-ext"],
 });
