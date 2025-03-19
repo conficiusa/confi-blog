8547ab2ea452b398627694312ebf18f17aa17137
conficiusa <addawebadua@gmail.com>
2025-03-19 18:09:56 +0000

    changed global  font to geist

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -17,106 +17,106 @@ import * as demo from "@/sanity/lib/demo";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
 import AuthProvider from "@/components/providers/sessionProvider";
 import TanstackProvider from "@/components/providers/tanstackProvider";
+import { Analytics } from "@vercel/analytics/react";
+import { geist } from "@/lib/fonts";
 
-// const inter = Inter({
-//   variable: "--font-inter",
-//   subsets: ["latin"],
-//   display: "swap",
-// });
 export async function generateMetadata(): Promise<Metadata> {
-  const settings = await sanityFetch({
-    query: settingsQuery,
-    // Metadata should never contain stega
-    stega: false,
-  });
-  const title = settings?.title || demo.title;
-  const description = settings?.description || demo.description;
+	const settings = await sanityFetch({
+		query: settingsQuery,
+		// Metadata should never contain stega
+		stega: false,
+	});
+	const title = settings?.title || demo.title;
+	const description = settings?.description || demo.description;
 
-  const ogImage = resolveOpenGraphImage(settings?.ogImage);
-  let metadataBase: URL | undefined = undefined;
-  try {
-    metadataBase = settings?.ogImage?.metadataBase
-      ? new URL(settings.ogImage.metadataBase)
-      : undefined;
-  } catch {
-    // ignore
-  }
-  return {
-    metadataBase,
-    title: {
-      template: `%s | ${title}`,
-      default: title,
-    },
-    description: toPlainText(description),
-    openGraph: {
-      images: ogImage ? [ogImage] : [],
-    },
-  };
+	const ogImage = resolveOpenGraphImage(settings?.ogImage);
+	let metadataBase: URL | undefined = undefined;
+	try {
+		metadataBase = settings?.ogImage?.metadataBase
+			? new URL(settings.ogImage.metadataBase)
+			: undefined;
+	} catch {
+		// ignore
+	}
+	return {
+		metadataBase,
+		title: {
+			template: `%s | ${title}`,
+			default: title,
+		},
+		description: toPlainText(description),
+		openGraph: {
+			images: ogImage ? [ogImage] : [],
+		},
+	};
 }
 export default async function RootLayout({
-  children,
-  modal,
+	children,
+	modal,
 }: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
+	children: React.ReactNode;
+	modal: React.ReactNode;
 }) {
-  const data = await sanityFetch({ query: settingsQuery });
-  const footer = data?.footer || [];
-  const { isEnabled: isDraftMode } = await draftMode();
+	const data = await sanityFetch({ query: settingsQuery });
+	const footer = data?.footer || [];
+	const { isEnabled: isDraftMode } = await draftMode();
 
-  return (
-    <html lang="en" className={cn(" bg-white text-black")}>
-
-      <AuthProvider>
-        <TanstackProvider>
-        <body className="container mx-auto sm:px-5 px-2 py-2 justify-end flex flex-col min-h-screen">
-          <div>
-            <section className="min-h-screen">
-              {isDraftMode && <AlertBanner />}
-              <header className="flex justify-end">
-                <Navbar />
-              </header>
-              <main>
-                {modal}
-                {children}
-              </main>
-              <footer className="bg-accent-1 border-accent-2 border-t">
-                <div className="container mx-auto sm:px-5 px:2">
-                  {footer.length > 0 ? (
-                    <PortableText
-                      className="prose-sm text-pretty bottom-0 w-full max-w-none bg-white py-12 text-center md:py-20"
-                      value={footer as PortableTextBlock[]}
-                    />
-                  ) : (
-                    <div className="flex flex-col items-center py-28 lg:flex-row">
-                      <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-                        Confi.dev
-                      </h3>
-                      <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-                        <a
-                          href="https://nextjs.org/docs"
-                          className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
-                        >
-                          Join the Community
-                        </a>
-                        <a
-                          href="https://github.com/vercel/next.js/tree/canary/examples/cms-sanity"
-                          className="mx-3 font-bold hover:underline"
-                        >
-                          View on GitHub
-                        </a>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </footer>
-            </section>
-            {isDraftMode && <VisualEditing />}
-            <SpeedInsights />
-          </div>
-        </body>
-        </TanstackProvider>
-      </AuthProvider>
-    </html>
-  );
+	return (
+		<html
+			lang='en'
+			className={cn(" bg-white text-black", geist.className)}
+		>
+			<AuthProvider>
+				<TanstackProvider>
+					<body className='container mx-auto sm:px-5 px-2 py-2 justify-end flex flex-col min-h-screen'>
+						<div>
+							<section className='min-h-screen'>
+								{isDraftMode && <AlertBanner />}
+								<header className='flex justify-end'>
+									<Navbar />
+								</header>
+								<main>
+									{modal}
+									{children}
+									<Analytics />
+								</main>
+								<footer className='bg-accent-1 border-accent-2 border-t'>
+									<div className='container mx-auto sm:px-5 px:2'>
+										{footer.length > 0 ? (
+											<PortableText
+												className='prose-sm text-pretty bottom-0 w-full max-w-none bg-white py-12 text-center md:py-20'
+												value={footer as PortableTextBlock[]}
+											/>
+										) : (
+											<div className='flex flex-col items-center py-28 lg:flex-row'>
+												<h3 className='mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl'>
+													Confi.dev
+												</h3>
+												<div className='flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4'>
+													<a
+														href='https://nextjs.org/docs'
+														className='mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8'
+													>
+														Join the Community
+													</a>
+													<a
+														href='https://github.com/vercel/next.js/tree/canary/examples/cms-sanity'
+														className='mx-3 font-bold hover:underline'
+													>
+														View on GitHub
+													</a>
+												</div>
+											</div>
+										)}
+									</div>
+								</footer>
+							</section>
+							{isDraftMode && <VisualEditing />}
+							<SpeedInsights />
+						</div>
+					</body>
+				</TanstackProvider>
+			</AuthProvider>
+		</html>
+	);
 }
